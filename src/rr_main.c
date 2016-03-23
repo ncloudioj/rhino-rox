99d6d28bd93aca91d6196ba810ddc99155bd67ca
Nan Jiang <njiang028@gmail.com>
2016-03-23 13:31:08 -0400

    Tweak makefile

--- src/rr_main.c
+++ src/rr_main.c
@@ -1,5 +1,6 @@
 #include "rr_rhino_rox.h"
 #include "rr_logging.h"
+#include "sds.h"
 
 #include <stdio.h>
 
@@ -7,7 +8,11 @@ int main(int argc, char *argv[]) {
     UNUSED(argc);
     UNUSED(argv);
 
-    rr_debug("Rhino-Rox is climbing the big tree now...");
+    sds s = sdsnew("hello");
+
+	rr_log(RR_LOG_DEBUG, s);
+	sdsfree(s);
+    rr_debug("%d Rhino-Rox is climbing the big tree now.", 10);
     rr_log(RR_LOG_DEBUG, "Rhino is %d years old golden snub-nosed monkey.", 12);
     return 0;
 }
